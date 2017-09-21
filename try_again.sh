9ff1145f9885cf5a084f608d6324a6b4b36356ab
Rick Carlino <rick.carlino@gmail.com>
2017-09-21 10:01:18 -0500

    HELP

---
+++ try_again.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/fish
+
+docker rm some-rabbit --force
+
+sudo docker run                          \
+     -p 15672:15672                      \
+     -p 5672:5672                        \
+     -p 3002:15675                       \
+     -p 8883:8883                        \
+     -p 1883:1883                        \
+     --hostname some-rabbit              \
+     --name some-rabbit                  \
+     -v (pwd)/conf:/etc/rabbitmq         \
+     -v (pwd)/rabbitmq:/var/lib/rabbitmq \
+     some-rabbit
