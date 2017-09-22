6e667008e46700f9aee617eab51f7bd6eedcbdc2
Connor Rigby <connor@farmbot.io>
2017-09-22 14:19:55 -0700

    :basketball:

--- jwt_plugin/src/rabbit_auth_backend_jwt_decoder.erl
+++ jwt_plugin/src/rabbit_auth_backend_jwt_decoder.erl
@@ -1,24 +1,31 @@
 -module(rabbit_auth_backend_jwt_decoder).
 
 -export([decode/1]).
+-export([find_alg/1]).
 
 decode(JwtBin) ->
   %% check alg header
 
   case binary:split(JwtBin, <<".">>, [global]) of
     [HeaderBin, _, _] ->
-      #{<<"typ">> := _Type, <<"alg">> := Alg} = maps:from_list(jsx:decode(base64:decode(HeaderBin))),
-      case Alg of
-        %% If it is the correct alg, move on.
-        <<"RS256">> ->
+      case find_alg(jsx:decode(base64:decode(HeaderBin))) of
+        {ok, <<"RS256">>} ->
           {ok, RSAKey} = rabbit_auth_backend_jwt_pub_key_fetcher:fetch(),
           case jwt:decode(JwtBin, RSAKey) of
             {ok, Claims} -> {ok, maps:get(<<"bot">>, Claims)};
             Err -> Err
           end;
-      %% Error if it isn't.
-        _other -> {error, "bad alg."}
+        {ok, _other} -> {error, "bad alg."};
+        {error, Reason} -> {error, Reason}
       end;
     _ -> {error, "bad jwt."}
-
   end.
+
+find_alg([{<<"alg">>, Alg} | _T]) ->
+  {ok, Alg};
+
+find_alg([_ | T]) ->
+  find_alg(T);
+
+find_alg([]) ->
+  {error, "No alg."}.
